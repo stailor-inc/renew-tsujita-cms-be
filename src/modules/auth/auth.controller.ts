c2fe2a21fa0de9faf2b407025240792d9afd5a31
Gelya (Jitera) <developers@jitera.com>
2024-06-19 09:53:39 +0000

    feat[api]: implement User Login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,7 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, HttpException, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, HttpException } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuditLogEntryDto } from './dtos/audit-log-entry.dto';
 import { LoginDto } from './dtos/login.dto';
 import { LoginResponseDto } from './dtos/login-response.dto';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AuthController {
@@ -23,6 +21,8 @@ export class AuthController {
         throw new HttpException({ status: HttpStatus.UNAUTHORIZED, error: error }, HttpStatus.UNAUTHORIZED);
       } else if (error === 'Incorrect login details') {
         throw new HttpException({ status: HttpStatus.BAD_REQUEST, error: error }, HttpStatus.BAD_REQUEST);
+      } else if (error === 'Password has expired, please reset your password.') {
+        throw new HttpException({ status: HttpStatus.FOUND, redirect: 'password-reset' }, HttpStatus.FOUND);
       }
     } else if (redirect) {
       response.redirect = redirect;
@@ -35,31 +35,5 @@ export class AuthController {
     }
 
     return response;
-  }
-
-  @Post('/audit_logs')
-  @HttpCode(HttpStatus.CREATED)
-  @UseGuards(AuthGuard('jwt'))
-  async writeAuditLog(@Body() auditLogEntryDto: AuditLogEntryDto): Promise<{ status: number; message: string }> {
-    const { user_id, timestamp, manipulate, params } = auditLogEntryDto;
-
-    if (!user_id || !await this.authService.userRepository.findOne({ where: { id: user_id } })) {
-      throw new HttpException({ status: HttpStatus.BAD_REQUEST, error: "User not found." }, HttpStatus.BAD_REQUEST);
-    }
-
-    if (!timestamp || isNaN(Date.parse(timestamp))) {
-      throw new HttpException({ status: HttpStatus.BAD_REQUEST, error: "Invalid timestamp." }, HttpStatus.BAD_REQUEST);
-    }
-
-    if (manipulate !== 'LOGIN_SUCCESS') {
-      throw new HttpException({ status: HttpStatus.BAD_REQUEST, error: "Action for the log entry is required." }, HttpStatus.BAD_REQUEST);
-    }
-
-    if (typeof params !== 'string') {
-      throw new HttpException({ status: HttpStatus.BAD_REQUEST, error: "Invalid parameters." }, HttpStatus.BAD_REQUEST);
-    }
-
-    await this.authService.writeAuditLogEntry(user_id, new Date(timestamp), manipulate, params);
-    return { status: HttpStatus.CREATED, message: "Audit log entry written successfully." };
   }
 }
